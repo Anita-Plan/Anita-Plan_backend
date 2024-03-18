e33293ff5c364a15b1e1c8fef4aa7298f33476be
Rui Duram <ruiduran.jr@gmail.com>
2024-03-18 14:38:12 +0100

    bug fix

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,4 +1,4 @@
-const express = require("express");
+/*const express = require("express");
 const router = express.Router();
 const User = require("../models/User.model");
 
@@ -21,6 +21,48 @@ router.put("/user/:id", async (req, res) => {
       return res.status(404).json({ message: "User not found" });
     }
     user.save();
+    res.json(user);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
+
+module.exports = router;*/
+
+const express = require("express");
+const router = express.Router();
+const User = require("../models/User.model");
+
+router.get("/user/:id", async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    res.json(user);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
+
+router.put("/user/:id", async (req, res) => {
+  try {
+    const userId = req.params.id;
+    const { planItem } = req.body;
+
+    let user = await User.findById(userId);
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    if (user.plan) {
+      user.plan.push(planItem);
+    } else {
+      user.plan = [planItem];
+    }
+
+    user = await user.save();
+
     res.json(user);
   } catch (error) {
     res.status(400).json({ error: error.message });
